c17b93ab942c2b886732c77af49ffa283fe5706b
Dolev Dublon <62290677+dolev146@users.noreply.github.com>
2021-12-31 13:10:00 +0200

    Pokemon.py

---
+++ Classes/Pokemon.py
@@ -0,0 +1,12 @@
+class Pokemon:
+    def __init__(self, value:float =None, type:int=None, pos:str=None) -> None:
+        self.value = value
+        self.type = type
+        self.pos = pos
+
+
+    def __str__(self) -> str:
+        return f""" pokemon(value= {self.value} , type= {self.type} , pos= {self.pos} \n"""
+
+    def __repr__(self) -> str:
+        return f""" pokemon(value= {self.value} , type= {self.type} , pos= {self.pos} \n"""
