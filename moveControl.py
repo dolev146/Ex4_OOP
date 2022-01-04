a286b6936e664a69ced1e90911f7294c2bce7e21
yakovkhodorkovski <yaakov103@gmail.com>
2022-01-04 12:41:53 +0200

    update on 12:41 tuesday 4 jan

--- moveControl.py
+++ moveControl.py
@@ -9,6 +9,7 @@ def decide_to_move():
         does not send over 10 times a move per second
     """
     if len(settings.movelist) > 0 :
-        for move in settings.movelist:
+        for move in list(settings.movelist):
             settings.client.choose_next_edge('{"agent_id":' + str(move["agent_id"]) + ', "next_node_id":' + str(move["next_node_id"]) + '}')
+            settings.movelist.remove(move)
         settings.client.move()
