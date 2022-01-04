8e10b0f8ce7f14ce18b75e301c1e285d7ec51ee6
yakovkhodorkovski <yaakov103@gmail.com>
2022-01-04 12:34:11 +0200

    update on 12:33 tuesday 4 jan

--- moveControl.py
+++ moveControl.py
@@ -1,3 +1,6 @@
+import settings
+
+
 def decide_to_move():
     """
         This function manages the sending of the client.move () function
@@ -5,4 +8,7 @@ def decide_to_move():
         In addition it should take into account the elapsed time so that it
         does not send over 10 times a move per second
     """
-    pass
+    if len(settings.movelist) > 0 :
+        for move in settings.movelist:
+            settings.client.choose_next_edge('{"agent_id":' + str(move["agent_id"]) + ', "next_node_id":' + str(move["next_node_id"]) + '}')
+        settings.client.move()
