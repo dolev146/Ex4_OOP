a31183dee8f23a63935fe074508009dbf2b1fb55
Dolev Dublon <62290677+dolev146@users.noreply.github.com>
2022-01-05 20:30:21 +0200

    finished gui

--- agentControl.py
+++ agentControl.py
@@ -11,16 +11,16 @@ def LengthAndPathFromPokemonToAgent(src: int, dest: int) -> (float, list):
 
 def control_agent(agent: Agent) -> int:
     # get Agent and return id node of the best pokemon next edge(idAgent,lengthOfThePath,listPath)
-    pokemonSaveDest=-1
+    pokemonSaveDest = -1
     bestTuple = (sys.maxsize, [])
     tempTuple = (sys.maxsize, [])
     for pokemon in list(settings.pokemons):
         tempTuple = LengthAndPathFromPokemonToAgent(agent.src, pokemon.win_node_src.id)
         if bestTuple[0] > tempTuple[0]:
             bestTuple = tempTuple
-            pokemonSaveDest=pokemon.win_node_dest.id
+            pokemonSaveDest = pokemon.win_node_dest.id
 
-    if len(bestTuple[1])==1:
+    if len(bestTuple[1]) == 1:
         return pokemonSaveDest
     return bestTuple[1][1]
 
@@ -35,7 +35,7 @@ def make_decisions():
     for agent in settings.agents:
         if agent.dest == -1:
             agent.dest = control_agent(agent)
-        #    settings.movelist.append({"agent_id": agent.id, "next_node_id": agent.dest})
+            #    settings.movelist.append({"agent_id": agent.id, "next_node_id": agent.dest})
             settings.client.choose_next_edge(
                 '{"agent_id":' + str(agent.id) + ', "next_node_id":' + str(agent.dest) + '}')
     settings.client.move()
